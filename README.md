dff3b5e6a1dd07fa5cc041ec449cda38bab64f5e
Aisha <aishaprograms@gmail.com>
2017-03-11 00:47:23 -0600

    updated styles, viewing divs in the game, readme

--- README.md
+++ README.md
@@ -1,5 +1,29 @@
-# week-4-game
+# Simple-RPG-Game
+
+## Understand
 This is a RPG game which involves an attack and counter attack strategy.
+
 This game uses Bootswatch theme Superhero and jQuery. The game itself is set up with an object prototype Character and DOM manipulations using jQuery. 
+
+* When the game starts, the player will choose a character by clicking on the characters's picture. The player will fight as that character for the rest of the game.
+
+* The player must then defeat all of the remaining fighters
+
+* The player chooses an opponent by clicking on an enemy's picture.
+
+* Once the player selects an opponent, that enemy is moved to a `defender area`.
+
+* The player will now be able to click the `attack` button.
+    * Whenever the player clicks `attack`, their character damages the defender. The opponent will lose `HP` (health points). These points are displayed at the bottom of the defender's picture. 
+    * The opponent character will instantly counter the attack. When that happens, the player's character will lose some of their `HP`. These points are shown at the bottom of the player character's picture.
+
+* The player will keep hitting the attack button in an effort to defeat their opponent. When the defender's HP is reduced to zero or below, the enemy is removed from the defender area. The player character can now choose a new opponent.
+
+* The player wins the game by defeating all enemy characters. The player loses the game the game if their character's HP falls to zero or below.
+
+## Credits
 Images obtained from http://www.ranker.com/list/list-of-all-uncanny-x-men-members/super-hero-teams
+
+
+## Heroku
 Heroku app: https://warm-hollows-56385.herokuapp.com/ 
--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,17 +1,5 @@
-//object contructor for Character
-function Character(name, image, health, attack, counterAttack) {
-    this.name = name;
-    this.image = image;
-    this.baseHealth = health;
-    this.health = health;
-    this.attack = attack;
-    this.baseAttack = attack;
-    this.counterAttack = counterAttack;
-    this.increaseAttack = increaseAttack;
-    this.decreaseHealth = decreaseHealth;
-    this.isLoser = isLoser;
-    this.resetCharacter = resetCharacter;
-}
+// =====================================================
+// CHARACTER METHODS AND CONSTRUCTOR
 
 //Each time the Character attacks, their character's Attack Power increases by its base Attack Power.
 function increaseAttack() {
@@ -34,6 +22,24 @@ function resetCharacter() {
     this.attack = this.baseAttack;
 }
 
+//object contructor for Character
+function Character(name, image, health, attack, counterAttack) {
+    this.name = name;
+    this.image = image;
+    this.baseHealth = health;
+    this.health = health;
+    this.attack = attack;
+    this.baseAttack = attack;
+    this.counterAttack = counterAttack;
+    this.increaseAttack = increaseAttack;
+    this.decreaseHealth = decreaseHealth;
+    this.isLoser = isLoser;
+    this.resetCharacter = resetCharacter;
+}
+
+// =====================================================
+// CHARACTER INSTANCES AND GLOBAL VARIABLES
+
 //new instances of Characters
 //global variables
 var beast = new Character('Beast', '../images/beast.jpeg', 100, 5, 5);
@@ -45,6 +51,8 @@ var enemy;
 var playerAttackPoints;
 var enemyAttackPoints;
 
+// =====================================================
+// CHARACTER SELECTION BASED ON CHARACTER-FIGURE CLICKED ON
 
 //returns Character object based on figure selected
 function setFighter(characterFigure) {
@@ -59,28 +67,16 @@ function setFighter(characterFigure) {
     }
 }
 
-//DOM modifiying function using jQuery
-//selects the player and defender using the modified classes of the figures
-//moves player, potential enemies, and defender to appropriate locations
-function selectFighters() {
-    $('.character-figure').on('click', function() {
-        if (!$(this).hasClass('enemy-figure') && !$(this).hasClass('player-figure')) {
-            selectPlayer(this);
-            moveEnemies(this);
-        } else if ($(this).hasClass('enemy-figure') && !$(this).hasClass('defender-figure')) {
-            selectDefender(this);
-        }
-    });
-}
+// =====================================================
+// CHOOSE PLAYER AND MAKE OTHERS ENEMIES
 
 //activated by clicking on character figure
 //chooses player
 function selectPlayer(characterFigure) {
     player = setFighter(characterFigure);
-    playerCharacter =
-        $(characterFigure).css('border', '2px green solid');
+    playerCharacter =$(characterFigure).css('border', '2px green solid');
     $('#player-instruction').html('Your Character');
-    $(characterFigure).insertAfter('#player-instruction');
+    $('#selected-player').append(characterFigure);
     $(characterFigure).addClass('player-figure');
 }
 
@@ -91,6 +87,8 @@ function moveEnemies(characterFigure) {
     $('.character-figure').not(characterFigure).addClass('enemy-figure');
 }
 
+// =====================================================
+// SELECT DEFENDER
 
 //activated by clicking on character image
 //chooses defender
@@ -102,27 +100,8 @@ function selectDefender(characterFigure) {
     $('#play-message').html('You selected to fight ' + enemy.name);
 }
 
-//activated by attack button click
-//added no enemy selected condition here
-//carries out attack functions, DOM manipulation, gameover, player win, and restart game
-//noDefender set to a boolean if there is no defender 
-var noDefender = !$('.character-figure').hasClass('defender-figure');
-
-function toggleFight() {
-    $('#attack-button').on('click', function() {
-        if (enemy === undefined || (defeatedEnemy() && noDefender) && !allDefeated()) {
-            $('#play-message').html('Enemy has not been selected!');
-        } else if (!defeatedPlayer()) {
-            showGamePlay();
-            fightersAttack();
-            showAttackHit();
-            gameOver();
-            defenderLost();
-        } else {
-            return;
-        }
-    });
-}
+// =====================================================
+// FIGHTING
 
 //player attacks enemy to decrease enemy hp
 //enemy counter attacks to decrease player hp
@@ -149,6 +128,9 @@ function showGamePlay() {
     $('#play-message').html(playerMove + enemyMove);
 }
 
+// =====================================================
+// PLAYER OR ENEMY LOST
+
 //true when enemy is defeated
 function defeatedEnemy() {
     return enemy.isLoser();
@@ -159,6 +141,9 @@ function defeatedPlayer() {
     return player.isLoser();
 }
 
+// =====================================================
+// GAME OVER
+
 //show reset
 //show gameover msg
 //show any characters that were hidden in previous rounds
@@ -167,30 +152,47 @@ function gameOver() {
         $('#play-message').html('Game over...');
         var resetButton = '<button type="button" class="btn btn-info" id="reset-button">Reset</button>';
         $('#reset').html(resetButton);
+        $('#fight-section').removeClass('show').addClass('hidden');
         reset();
         $('.character-figure').show();
     }
 }
 
+// =====================================================
+// RESET
+
 //resets all Character properties which were changed and resets the DOM
 function reset() {
     $('#reset-button').on('click', function() {
+        // reset html
         $('#player-instruction').html('Choose a Player');
         $('.character-figure').appendTo('#characters');
         $('.character-figure').removeClass('player-figure enemy-figure defender-figure');
         $('.character-figure').css('border', '2px yellow solid');
         $('#play-message').html('');
         $('#reset').html('');
+        // reset characters
         beast.resetCharacter();
         phoenix.resetCharacter();
         magneto.resetCharacter();
         wolverine.resetCharacter();
+        // change figure captions
         $('#' + beast.name + '-figure-caption').html(beast.name + ' ' + beast.baseHealth + ' HP');
         $('#' + phoenix.name + '-figure-caption').html(phoenix.name + ' ' + phoenix.baseHealth + ' HP');
         $('#' + magneto.name + '-figure-caption').html(magneto.name + ' ' + magneto.baseHealth + ' HP');
         $('#' + wolverine.name + '-figure-caption').html(wolverine.name + ' ' + wolverine.baseHealth + ' HP');
+        // hide divs
+        $('#defender').removeClass('show').addClass('hidden');
+        $('#enemies').removeClass('show').addClass('hidden');
+        $('#fight-section').removeClass('show').addClass('hidden');
+        $('#game-play').removeClass('show').addClass('hidden');
+        $('#reset').removeClass('show').addClass('hidden');
+        $('#selected-player').removeClass('col-sm-6').addClass('col-sm-12');
     });
 }
+
+// =====================================================
+// PLAYER WINS
 
 //message saying you defeated character
 //message to choose new enemy
@@ -200,15 +202,22 @@ function defenderLost() {
         $('#play-message').html(message);
         $('.defender-figure').hide();
         $('.defender-figure').removeClass('defender-figure');
+        $('#enemies').removeClass('hidden').addClass('show');
+        $('#fight-section').removeClass('show').addClass('hidden');
         if (allDefeated()) {
             $('#play-message').html('Congratulations! You defeated all enemies.');
             var resetButton = '<button type="button" class="btn btn-info" id="reset-button">Reset</button>';
             $('#reset').html(resetButton);
+            $('#enemies').removeClass('show').addClass('hidden');
+            $('#fight-section').removeClass('show').addClass('hidden');
             reset();
             $('.character-figure').show();
         }
     }
 }
+
+// =====================================================
+// PLAYER DEFEATS ALL
 
 //loops through all figures which have a hidden class of enemy-figure and collects a total
 //if total is 3, all enemies have been defeated
@@ -219,6 +228,51 @@ function allDefeated() {
     });
     return total === 3;
 }
+
+// =====================================================
+// MAIN METHODS
+
+//DOM modifiying function using jQuery
+//selects the player and defender using the modified classes of the figures
+//moves player, potential enemies, and defender to appropriate locations
+function selectFighters() {
+    $('.character-figure').on('click', function() {
+        if (!$(this).hasClass('enemy-figure') && !$(this).hasClass('player-figure')) {
+            selectPlayer(this);
+            moveEnemies(this);
+            $('#enemies').removeClass('hidden').addClass('show');
+        } else if ($(this).hasClass('enemy-figure') && !$(this).hasClass('defender-figure')) {
+            selectDefender(this);
+            // hide divs
+            $('#enemies').removeClass('show').addClass('hidden');
+            $('#defender').removeClass('hidden').addClass('show');
+            $('#selected-player').removeClass('col-sm-12').addClass('col-sm-6');
+            $('#fight-section').removeClass('hidden').addClass('show');
+            $('#game-play').removeClass('hidden').addClass('show');
+        }
+    });
+}
+
+//activated by attack button click
+//added no enemy selected condition here
+//carries out attack functions, DOM manipulation, gameover, player win, and restart game
+//noDefender set to a boolean if there is no defender 
+var noDefender = !$('.character-figure').hasClass('defender-figure');
+
+function toggleFight() {
+    $('#attack-button').on('click', function() {
+        showGamePlay();
+        fightersAttack();
+        showAttackHit();
+        $('#reset').removeClass('hidden').addClass('show');
+        gameOver();
+        defenderLost();
+    });
+}
+
+
+// =====================================================
+// RENDER DOM
 
 //manipulates the DOM to display selected Characters and the effect of pressing the attack button
 function renderDom() {
